eb7281cd17d8bc461666a72a35476fadc740c779
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-11 18:45:05 +0530

    time display component created

--- src/components/author/view_time.js
+++ src/components/author/view_time.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+
 export default class ViewTime extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  componentDidMount(){
+    setInterval(() =>
+      this.setState({
+        date: new Date(),
+      })
+    ,1000)
+  }
+
+
   render(){
-    return (
+    return(
       <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
       </div>
-    );
+    )
   }
 }
