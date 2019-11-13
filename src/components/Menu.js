9944e4967d09d8e26610b0217c729ba9aa24bc74
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-13 16:09:04 +0530

    minor check with select tag state changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -39,7 +39,7 @@ class Menu extends Component{
 	        </div>
 					<div>
 						<Switch>
-							<Route  exact path="/"  component = {Home} />
+							<Route  exact path="/"  component = {FlavorForm} />
 							<Route  exact path="/authors"  component = {Authors} />
 							<Route  exact path="/authors/new"  component = {NewAuthor} />
 							<Route  exact path="/view"  component = {View} />
@@ -59,6 +59,41 @@ function Home() {
 			Welcome to react client page(Home page).
 		</div>
 		);
+}
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default Menu;
